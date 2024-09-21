43a8a38e0c7ade9036a78a721946d619e3e00600
Bogdan Milovanović <milovanovicbogdan6@gmail.com>
2024-09-21 20:13:41 +0200

    Done all to the Post, except id selctors

--- test.js
+++ test.js
@@ -18,20 +18,61 @@ const wdOpts = {
 async function runTest() {
   const driver = await remote(wdOpts);
   try {
-    console.log(chalk.bold.blue('Pressing Home Button'));
+    console.log(chalk.bold.blue('Pressing Home Button')); // log
     await driver.pressKeyCode(3);
-    const tikTokButton = await driver.$('~TikTok');
-    await tikTokButton.click();
-    countFrom(5);
-    const imageViewElement = await driver.$(
+    await driver.$('~TikTok').click();
+    countFrom(6);
+    const createButton = await driver.$(
       '//android.widget.Button[@content-desc="Create"]'
     );
-    await imageViewElement.click();
-    console.log(chalk.bold.blue('Clicking the Album Button'));
-    const touchAlbum = await driver.executeScript('mobile: clickGesture', [
-      { x: 1165, y: 2137 },
-    ]);
-    // await touchAlbum.click();
+    if (await createButton.isExisting()) {
+      await createButton.click();
+    }
+    countFrom(6);
+
+    console.log(chalk.bold.blue('Clicking the Album Button')); // log
+    await driver.executeScript('mobile: clickGesture', [{ x: 1165, y: 2137 }]);
+    countFrom(6);
+
+    console.log(chalk.bold.blue('Clicking the All⮟ Button')); // log
+    await driver
+      .$(
+        '//android.widget.LinearLayout[@resource-id="com.zhiliaoapp.musically:id/bvz"]'
+      )
+      .click();
+    countFrom(6);
+
+    console.log(chalk.bold.blue('Clicking the PendingUploads Folder')); // log
+    await driver
+      .$(
+        '//android.widget.TextView[@resource-id="com.zhiliaoapp.musically:id/gaj" and @text="PendingUploads"]'
+      )
+      .click();
+    countFrom(6);
+
+    console.log(chalk.bold.blue('Clicking the first video in grid')); // log
+    await driver
+      .$(
+        '(//android.widget.ImageView[@resource-id="com.zhiliaoapp.musically:id/irb"])[1]'
+      )
+      .click();
+    countFrom(6);
+
+    console.log(chalk.bold.blue('Clicking the Next button')); // log FIRST NEXT
+    await driver.$('#com.zhiliaoapp.musically:id/jfq').click();
+    countFrom(6);
+
+    console.log(chalk.bold.blue('Clicking the second Next')); // log SECOND NEXT
+    await driver.$('#com.zhiliaoapp.musically:id/jfz').click();
+    countFrom(6);
+
+    console.log(chalk.bold.blue('Clicking the second Next')); // log SECOND NEXT
+    await driver.$('#com.zhiliaoapp.musically:id/jfz').click();
+    countFrom(6);
+
+    console.log(chalk.bold.blue('Clicking the Post')); // log POST
+    await driver.$('#com.zhiliaoapp.musically:id/ldf').click();
+    countFrom(6);
   } finally {
     await driver.pause(1000);
     await driver.deleteSession();
